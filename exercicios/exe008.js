19b01731d55e96d1a851a6035b7d7499e1e25137
Denise Flora <98910258+deniseflora@users.noreply.github.com>
2023-02-26 11:56:01 -0300

    Atualização dos exercícios

---
+++ exercicios/exe008.js
@@ -0,0 +1,12 @@
+/*console.log("O console funcionou corretamente!");
+F8 ou CTRL+ALT+N*/
+var vel = 78.2
+console.log( `A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) {  //Condição Simples ! (não tem else/senão)
+    console.log( `Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
+
+
+
+
---
+++ exercicios/exe009.js
@@ -0,0 +1,8 @@
+var país = 'França'
+console.log(`Vivendo em ${país}`)
+if (país != 'Brasil') {
+   console.log('Você é Estrangeiro!')
+//Condição Composta! Quando têm else/senão
+}else {
+   console.log('você é Brasileiro!') 
+}
